1af61b44b4b1181cba66e022372bd36ea218f03f
Denis <dstjohn@outlook.com>
2021-03-10 07:16:46 -0600

    Completed project

--- src/Driver.py
+++ src/Driver.py
@@ -1,7 +1,5 @@
 import numpy as np
 import math
-import matplotlib.pyplot as plt
-import pandas as pd
 
 def main():
     #Test Sentences
@@ -10,35 +8,35 @@ def main():
     sentences.append("i went to school for a computer science convention.")
     sentences.append("i took a long drive last night to clear my mind")
     sentences.append("i spent my time watching tv while i was working")
-    sentences.append("computers are my favorite puppy they are they")
+    sentences.append("computers are my favorite puppy they are they are")
     
-    problem1(sentences)
-    problem2(sentences)
+    problem(sentences, 'BinaryData.csv', convert_to_keywords_binary, bayes_classification)
+    problem(sentences, 'NonBinaryData.csv', convert_to_keywords_count, bayes_continuous_classification)
 
-def problem1(sentences):
-    biniary_dataset = np.genfromtxt('BinaryData.csv', delimiter=',', skip_header=True, usecols=(0, 1, 2, 3, 4, 5, 6))
+def problem(sentences, file_name, keyword_function, classification_function):
+    dataset = np.genfromtxt(file_name, delimiter=',', skip_header=True, usecols=(0, 1, 2, 3, 4, 5, 6))
     
     for sentence in sentences:
-        keywords_present = convert_to_keywords_binary(sentence)
-        result_binary = bayes_classification(biniary_dataset, keywords_present)
+        keywords_present = keyword_function(sentence)
+        result_binary = classification_function(dataset, keywords_present, True)
         print(sentence)
         print(f"is sentence cs? {result_binary}")
         print()
-        
-    
+    mismatch = calculate_training_mismatch(dataset, classification_function, False)
+    print(f"mismatch percentage: {mismatch}%")
+    print()
 
-def problem2(sentences):
-    non_biniary_dataset = np.genfromtxt('NonBinaryData.csv', delimiter=',', skip_header=True, usecols=(0, 1, 2, 3, 4, 5, 6))
+def calculate_training_mismatch(dataset, class_function, print_flag):
+    class_index = np.shape(dataset)[1]-1
     
-    for sentence in sentences:
-        keywords_count = convert_to_keywords_count(sentence)
-        result_count = bayes_continuous_classification(non_biniary_dataset, keywords_count)
-        print(sentence)
-        print(f"is sentence cs? {result_count}")
-        print()
+    count = 0
+    for row_idx in range(np.shape(dataset)[0]):
+        result_count = class_function(dataset, dataset[row_idx,:class_index-1], print_flag)
+        if result_count == dataset[row_idx, class_index]:
+            count += 1
+    return (1 - (count / np.shape(dataset)[0])) * 100
     
-    
-def bayes_classification(dataset, keywords):
+def bayes_classification(dataset, keywords, print_flag):
     class_index = np.shape(dataset)[1]-1
     cs_dataset = dataset[dataset[:, class_index] > 0]
     cs = bayes_classification_inner(dataset, keywords, cs_dataset, class_index)
@@ -46,8 +44,9 @@ def bayes_classification(dataset, keywords):
     non_cs_dataset = dataset[dataset[:,class_index]==0]
     non_cs = bayes_classification_inner(dataset, keywords, non_cs_dataset, class_index)
     
-    print(f"cs:\t{cs}")
-    print(f"non_cs:\t{non_cs}")
+    if print_flag:
+        print(f"cs:\t{cs}")
+        print(f"non_cs:\t{non_cs}")
     
     return cs > non_cs
     
@@ -68,7 +67,7 @@ def bayes_classification_inner(dataset, keywords, cs_dataset, class_index):
     
     return class_cond * prior
 
-def bayes_continuous_classification(dataset, keywords):
+def bayes_continuous_classification(dataset, keywords, print_flag):
     class_index = np.shape(dataset)[1]-1
     cs_dataset = dataset[dataset[:, class_index] > 0]
     cs = bayes_continuous_classification_inner(dataset, keywords, cs_dataset, class_index)
@@ -76,8 +75,9 @@ def bayes_continuous_classification(dataset, keywords):
     non_cs_dataset = dataset[dataset[:,class_index]==0]
     non_cs = bayes_continuous_classification_inner(dataset, keywords, non_cs_dataset, class_index)
     
-    print(f"cs:\t{cs}")
-    print(f"non_cs:\t{non_cs}")
+    if print_flag:
+        print(f"cs:\t{cs}")
+        print(f"non_cs:\t{non_cs}")
     
     return cs > non_cs
 
